d34a1c3797b8a09d450e868fab33a89ea4ab7df5
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-10 16:54:45 -0700

    print without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing using system()
+ *
+ * Return: Always 1 (Not Success)
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
