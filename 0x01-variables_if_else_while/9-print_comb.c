83cef51b3839c485a16c14fd112c1293957ee116
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-11 05:06:24 -0700

    correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 {
 	int i;
 
-	putchar('0');
-	for (i = 1; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(' ');
-		putchar(',');
 		putchar('0' + i);
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
