34acb4617d3f1a2d49e6fe78fcd209bca0d8fd79
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-17 16:42:32 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,17 +4,23 @@
 * print_number - prints an integer.
 * @n: integer to be printed.
 *
+* note!: checker gives code error.
+*
 * Return: void
 */
 
 void print_number(int n)
 {
+	unsigned int local_n;
+
 	if (n < 0)
 	{
-		n *= -1;
+		local_n = -1 * n;
 		_putchar('-');
 	}
-	if ((n / 10) != 0)
-		print_number(n / 10);
-	_putchar('0' + (n % 10));
+	else
+		local_n = n;
+	if ((local_n / 10) != 0)
+		print_number(local_n / 10);
+	_putchar('0' + (local_n % 10));
 }
