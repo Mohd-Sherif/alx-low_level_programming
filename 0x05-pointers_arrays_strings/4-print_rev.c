6b3867807b6609099c63ef7da663ea4c6cbbffd8
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-21 09:29:24 -0700

    print in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* print_rev - prints a string, in reverse, followed by a new line.
+* @s: pointer to the first address of the string.
+*
+* Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s); i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
+/**
+* _strlen - returns the length of a string.
+* @s: pointer to the first address of the string.
+*
+* Return: length of the given string.
+*/
+
+int _strlen(char *s)
+{
+	int strlen = 0;
+
+	while (*s != '\0')
+	{
+		strlen++;
+		s++;
+	}
+	return (strlen);
+}
