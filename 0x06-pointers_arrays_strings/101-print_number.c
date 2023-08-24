a5cf655d94c33e9d812e49387800cc62978fad77
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-24 05:10:23 -0700

    add unsigned int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,12 +9,14 @@
 
 void print_number(int n)
 {
+	unsigned int x = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = -1 * n;
 	}
-	if (n / 10 != 0)
-		print_number(n / 10);
-	_putchar('0' + (n % 10));
+	if (x / 10 != 0)
+		print_number(x / 10);
+	_putchar('0' + (x % 10));
 }
