8b551f41f5b7d2c0cc01d1638c398d0347e13ae3
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-08-24 04:46:40 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* rot13 - encodes a string using rot13.
+* @str: pointer to the first address of the array.
+*
+* Return: pointer to char
+*/
+
+char *rot13(char *str)
+{
+	int i, j;
+	char alpha[] = {
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+		'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+		's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+	};
+	char rot13_mapping[] = {
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
+		'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		'J', 'K', 'L', 'M', 'n', 'o', 'p', 'q', 'r', 's', 't',
+		'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e',
+		'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
+	};
+
+	for (i = 0; str[i] != '\0'; i++)
+		for (j = 0; alpha[j] != '\0'; j++)
+			if (str[i] == alpha[j])
+			{
+				str[i] = rot13_mapping[j];
+				break;
+			}
+	return (str);
+}
