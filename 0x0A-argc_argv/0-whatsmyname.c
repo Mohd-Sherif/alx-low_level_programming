da2731d77edc6a7596d11fb37bcc9a2aec2e9f3e
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-01 07:03:59 -0700

    what's my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - main entry
+ *
+ * Description: prints its name, followed by a new line.
+ *
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
