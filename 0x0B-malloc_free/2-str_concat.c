5fa7f493b409668b1461040e487013ce0e68ed00
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-04 04:46:08 -0700

    add checks if one string only is equal to null

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	else if (s1 != NULL && s2 == NULL)
+		return (s1);
+	else if (s1 == NULL && s2 != NULL)
+		return (s2);
 	/* length calcultion */
 	while (s1[len1] != '\0')
 		len1++;
