853f596aee684ed9886327b0da3fa66315f6ad0f
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-11 05:54:51 -0700

    add dog header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/*
+ * struct dog - a dog struct contains its properties.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H_ */
