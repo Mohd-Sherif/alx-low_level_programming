9d56b6626d14ef8929ae3eef6ed0e2947eae4806
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-11 05:28:38 -0700

    resolving betty errors

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H_
 #define FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) ((x) < 0? (x) * -1: (x))
+#define ABS(x) ((x) < 0 ? (x) * -1 : (x))
 
 #endif /* FUNCTION_LIKE_MACRO_H_ */
