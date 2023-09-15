4bcba240517e81b9fc658250f9021cc9dd03895e
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-15 03:31:15 -0700

    check for separator if NULL in the loop

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,11 +13,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list ap;
 
-	if (n == 0 || separator == NULL)
+	if (n == 0)
 		return;
 	va_start(ap, n);
 	for (i = 0; i < n - 1; i++)
-		printf("%i%s", va_arg(ap, int), separator);
+		if (separator != NULL)
+			printf("%i%s", va_arg(ap, int), separator);
+		else
+			printf("%i", va_arg(ap, int));
 	printf("%i\n", va_arg(ap, int));
 	va_end(ap);
 }
