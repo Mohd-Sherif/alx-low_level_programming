bf978bb8fd517ffd7dd674bad006e49e065299c3
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-21 06:12:15 -0700

    print a statement before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * _constructor - executes before main().
+ *
+ * Return: void
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
