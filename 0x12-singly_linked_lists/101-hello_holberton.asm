b9429d120180bd1455fa7e5026a79538c70114b3
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-21 06:27:56 -0700

    hello assembly

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+section .data
+	msg db 'Hello, Holberton', 0xA, 0 ; String to be printed
+
+section .text
+	global main
+	extern printf
+
+main:
+	; Write the string to stdout
+	mov rax, 1              ; File descriptor 1 is stdout
+	mov rdi, 1              ; Buffer address
+	mov rsi, msg            ; Address of the string
+	mov rdx, 18             ; Length of the string
+	syscall
+
+	; Exit the program
+	mov eax, 60             ; System call number for exit
+	xor edi, edi            ; Exit status (0)
+	syscall
