04933a456e459ff0fd253a65866711890a75fd23
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-25 03:32:48 -0700

    add node int

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H_
 #define LISTS_H_
 
+#include <stddef.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -16,5 +19,7 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* LISTS_H_ */
