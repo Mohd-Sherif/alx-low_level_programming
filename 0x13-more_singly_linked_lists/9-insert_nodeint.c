e40343bcea6679f5690d941bd9e71bde22bc9a1a
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-25 10:53:44 -0700

    check for NULL

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *tmp, *old = *head;
 	unsigned int ctr = 0;
 
-	if (old == NULL)
+	if (old == NULL || head == NULL)
 		return (NULL);
 	else if (idx == 0)
 		return (add_nodeint(head, n));
