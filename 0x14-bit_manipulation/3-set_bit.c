6c0149a9f48183eabf3d2859431ae8300322fed5
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-09-29 09:11:55 -0700

    handle index out of range

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,7 +9,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (n == NULL)
+	if ((n == NULL) || (index > (sizeof(n) * CHAR_BIT)))
 		return (-1);
 	*n |= (1UL << index);
 	return (1);
