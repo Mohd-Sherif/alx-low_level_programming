ee6ab491f3530d75f6a433964ebecf3378c9b90b
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-02 14:36:43 -0700

    resolve compilation error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,8 +29,8 @@ int _strlen(char *s)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fileDescriptor;
-	size_t textLength = _strlen(text_content), writeLetters;
+	int fileDescriptor, writeLetters;
+	int textLength = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
