bd3f2a7199b036eab87ca736472ac4aac3152036
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-02 14:29:03 -0700

    add text_content NULL check

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,12 +37,13 @@ int create_file(const char *filename, char *text_content)
 	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fileDescriptor == -1)
 		return (-1);
-	if (text_content != NULL)
-		if (write(fileDescriptor, text_content, textLength) == -1)
-		{
-			close(fileDescriptor);
-			return (-1);
-		}
+	if (text_content == NULL)
+		text_content = "";
+	if (write(fileDescriptor, text_content, textLength) == -1)
+	{
+		close(fileDescriptor);
+		return (-1);
+	}
 	close(fileDescriptor);
 	return (1);
 }
