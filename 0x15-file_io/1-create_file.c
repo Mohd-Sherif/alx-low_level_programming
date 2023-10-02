e3175fdbaa4fd49780a5bc53f2028d540f23f517
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-02 14:24:01 -0700

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to the first address of the string.
+ *
+ * Return: length of the given string.
+ */
+int _strlen(char *s)
+{
+	int strlen = 0;
+
+	while (*s != '\0')
+	{
+		strlen++;
+		s++;
+	}
+	return (strlen);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: the name of the file to create.
+ * @text_content: a NULL terminated string to write to the file.
+ * Return: 1 on success, -1 on failure
+ *(file can not be created, file can not be written, write “fails”, etc…)
+ *      if filename is NULL return -1.
+ *      if text_content is NULL create an empty file.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDescriptor;
+	size_t textLength = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fileDescriptor == -1)
+		return (-1);
+	if (text_content != NULL)
+		if (write(fileDescriptor, text_content, textLength) == -1)
+		{
+			close(fileDescriptor);
+			return (-1);
+		}
+	close(fileDescriptor);
+	return (1);
+}
