3f71b555d85cc4559d0a3c8b68fc7cb20e64c86b
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-02 13:38:11 -0700

    add test case for letters size passed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t readLetters, writeLetters;
 	char *buffer;
 
-	if (filename == NULL)
+	if (filename == NULL || !letters)
 		return (0);
 	buffer = malloc(sizeof(char) * letters + 1);
 	if (buffer == NULL)
