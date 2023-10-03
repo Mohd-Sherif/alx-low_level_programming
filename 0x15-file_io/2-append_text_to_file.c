628dceeff708165556fbe6857b0607492a76e9bd
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-10-03 01:26:10 -0700

    trial to fix NULL input

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -34,7 +34,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (!text_content)
 		return (1);
 	fileDescriptor = open(filename, O_WRONLY | O_APPEND);
 	if (fileDescriptor == -1)
