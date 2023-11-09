ad8deb5a9b58ee925cfdf778aa2f22be3166f55d
Mohd-Sherif <Mohd-Sherif@outlook.com>
2023-11-09 14:02:00 -0800

    Add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @head: pointer to the head of the list.
+ * @n: integer
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return NULL;
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	if (head == NULL || *head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	(*head)->prev = new;
+	*head = new;
+	return (*head);
+}
