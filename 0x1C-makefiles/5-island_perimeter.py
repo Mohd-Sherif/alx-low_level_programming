285905168f5b6d331d002954473f749c178e6087
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-01-24 14:52:23 -0800

    rearrange functions

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,15 +1,6 @@
 #!/usr/bin/python3
 """Module that calculates the perimeter of an island in a grid."""
 
-def island_perimeter(grid):
-    """Calculates the perimeter of the island in grid."""
-    p = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            if grid[i][j] == 1:
-                p += water_neighbors(grid, i, j)
-    return p
-
 def water_neighbors(grid, i, j):
     """Calculates the number of water neighbors a cell has in a grid."""
     n = 0
@@ -27,3 +18,12 @@ def water_neighbors(grid, i, j):
         n += 1
 
     return n
+
+def island_perimeter(grid):
+    """Calculates the perimeter of the island in grid."""
+    p = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                p += water_neighbors(grid, i, j)
+    return p
