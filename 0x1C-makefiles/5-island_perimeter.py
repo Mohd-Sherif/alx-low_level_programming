b6b46e73c278e2881a0a2170b672d059bf09a3b8
Mohd-Sherif <Mohd-Sherif@outlook.com>
2024-01-24 14:48:10 -0800

    trial 1

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,17 +20,17 @@ def water_neighbors(grid, i, j):
         Calculates the number of water neighbors a cell has in a grid.
     """
     n = 0
-    
-    if i <= 0 or grid[i - 1][j] == 0:
-        n += 1
-    
-    if j <= 0 or grid[i - 1][j] == 0:
+
+    if i <= 0 or not grid[i - 1][j]:
         n += 1
 
-    if j >= len(grid[i]) - 1 or grid[i][j + 1] == 0:
+    if j <= 0 or not grid[i - 1][j]:
         n += 1
 
-    if i >= len(grid) - 1 or grid[i + 1][j] == 0:
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        n += 1
+
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
         n += 1
 
     return n
